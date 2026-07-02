efe8bb32935fcb140664f8e23fd8194f4882d067
https://github.com/sa7028894-arch <sa7028894@gmail.com>
2026-07-02 23:41:48 +0530

    Initial commit for PackVault

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [packages, setPackages] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/list-packages')
+      .then(res => setPackages(res.data))
+      .catch(err => console.error("Error fetching packages:", err));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: '20px' }}>
+      <h1>PackVault Dashboard</h1>
+      <table border="1" cellPadding="10">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Version</th>
+            <th>Path</th>
+          </tr>
+        </thead>
+        <tbody>
+          {packages.map(pkg => (
+            <tr key={pkg.id}>
+              <td>{pkg.name}</td>
+              <td>{pkg.version}</td>
+              <td>{pkg.local_path}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
